9b257d1b7f192c1246a681f7ff80ab0f580e13df
Marcus Lull <marcuslull@gmail.com>
2024-04-28 10:07:08 -0700

    #15 - Unit tests for authentication and authorization events

---
+++ src/test/java/com/marcuslull/auth/listeners/AuthenticationEventTest.java
@@ -0,0 +1,84 @@
+package com.marcuslull.auth.listeners;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationEventTest {
+
+    // STRATEGY - The most direct route to test logs seems to be a comparison, so we will capture the log as it fires
+    // and compare it to what was outputted by the methods under test.
+
+    @Mock
+    private AuthenticationSuccessEvent authenticationSuccessEvent;
+
+    @Mock
+    private AbstractAuthenticationFailureEvent abstractAuthenticationFailureEvent;
+
+    @InjectMocks
+    private AuthenticationEvent authenticationEvent;
+
+    // the captured log will be stored here for the comparison
+    private ListAppender<ILoggingEvent> listAppender;
+
+    @BeforeEach
+    void setUp() {
+        // fire off the event
+        authenticationEvent = new AuthenticationEvent();
+
+        // create a context and get the logger from the applicable module
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        ch.qos.logback.classic.Logger eventLogger =
+                loggerContext.getLogger("com.marcuslull.auth.listeners.AuthenticationEvent");
+
+        // add the captured log to the list
+        listAppender = new ListAppender<>();
+        listAppender.start();
+        eventLogger.addAppender(listAppender);
+    }
+
+    @Test
+    void onSuccess() {
+        // arrange
+
+        // act
+        authenticationEvent.onSuccess(authenticationSuccessEvent);
+
+        // assert
+        ILoggingEvent loggingEvent = listAppender.list.getFirst();
+        assertEquals("EVENT: AuthenticationEvent.onSuccess(authenticationSuccessEvent)", loggingEvent.getFormattedMessage());
+        assertEquals(Level.WARN, loggingEvent.getLevel());
+    }
+
+    @Test
+    void onFailure() {
+        // arrange
+
+        // act
+        authenticationEvent.onFailure(abstractAuthenticationFailureEvent);
+
+        // assert
+        ILoggingEvent loggingEvent = listAppender.list.getFirst();
+        assertEquals("EVENT: AuthenticationEvent.onFailure(abstractAuthenticationFailureEvent)", loggingEvent.getFormattedMessage());
+        assertEquals(Level.WARN, loggingEvent.getLevel());
+    }
+}
